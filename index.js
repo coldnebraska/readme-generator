4fd87bf794ee297779e682ae30e2f62f2cf3ebc3
DBrush <coldnebraska@gmail.com>
2023-11-15 18:18:54 -0600

    added questions for prompt

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project", "Please enter a description for your project", "Please enter any installation instructions", "Please provide any usage information", "Please provide any contribution guidelines", "Please provide testing instructions"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
