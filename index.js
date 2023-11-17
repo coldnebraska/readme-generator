964c76687fd5e2222a12d94630007ae915c2d1ab
DBrush <coldnebraska@gmail.com>
2023-11-16 18:11:38 -0600

    removed todo comment

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let badge = ""
 const questions = ["What is the title of your project:", "Please enter a description for your project:", "Please enter any installation instructions:", "Please provide information on how to use this app:", "Please list any contributors:", "Please provide testing instructions:", "What is your Github username?", "What is your email address?"];
 const badgeChoices = ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU General Public License v2.0", "GNU Affero General Public License v3.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, 
 
